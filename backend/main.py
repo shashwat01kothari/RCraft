ca9f4232f6038e1124c699708a41ed81ab44f794
Shashwat Kothari <shashwat01kothari@gmail.com>
2025-10-13 11:22:09 +0530

    added optimizer to main

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,26 @@
 from fastapi import FastAPI
 import uvicorn
 
-from backend.core.apis.analysis_router import router as  analysis_router
+from .core.apis.analysis_router import router as  analysis_router
+from backend.core.apis.optimizer_router import router as optimizer_router
 
 app = FastAPI(
-    title="Career Co-Pilot API",
+    title="ResumeCraft.ai",
     description="An AI-powered service to generate a holistic analysis of resumes.",
-    version="2.0.0"
+    version="1.0.0"
 )
 
 # Include the router that contains our /analyze endpoint
-app.include_router(analysis_router, prefix="/api", tags=["Analysis"])
+app.include_router(
+    analysis_router, 
+    prefix="/api/v1", 
+    tags=["Analysis"])
+
+app.include_router(
+    optimizer_router, 
+    prefix="/api/v1", 
+    tags=["Optimizer"]
+)
 
 @app.get("/", tags=["Root"])
 async def read_root():
@@ -20,10 +30,13 @@ async def read_root():
 
 # This allows running the server directly using `python main.py`
 if __name__ == "__main__":
-     uvicorn.run(
+    uvicorn.run( 
         "backend.main:app", 
-        host="127.0.0.0", 
+        host="127.0.0.1", 
         port=8000, 
         reload=True,
         reload_dirs=["backend"] 
     )
+     
+#   uvicorn backend.main:app --host 127.0.0.1 --port 8000 --reload --reload-dir backend
+
